155bd32bbad9dc258e431cc8d7b660c77ae779de
Jettsyy <teazyyy2512@gmail.com>
2025-05-22 12:44:01 +0700

    feat: UI for create product

--- src/Layouts/Header.tsx
+++ src/Layouts/Header.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router";
 
 const Header = () => {
   return (
@@ -78,20 +79,20 @@ const Header = () => {
                   </a>
                 </li>
                 <li>
-                  <a
-                    href="#"
+                  <Link
+                    to="/products"
                     className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                   >
-                    List Product
-                  </a>
+                    List product
+                  </Link>
                 </li>
                 <li>
-                  <a
-                    href="#"
+                  <Link
                     className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                    to="product-create"
                   >
                     Create Product
-                  </a>
+                  </Link>
                 </li>
                 <li>
                   <a
--- src/components/FormCreateProduct.tsx
+++ src/components/FormCreateProduct.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
 
 const FormCreateProduct = () => {
+  // Thêm sản phẩm
+  // 1. Tạo form nhập thông tin sản phẩm
+  // 2. Láy dữ liệu từ form và validate dữ liệu
+  // 3. Gửi dữ liệu lên server
   return (
-    <div>FormCreateProduct</div>
-  )
-}
+    <div className="w-[500px] mx-auto mt-10">
+      <h1 className="text-center text-2xl font-semibold">Create Product</h1>
+      <form action="" onSubmit={}>
+        <div className="mt-2">
+          <label className="font-medium mb-2 block" htmlFor="productName">
+            Product Name
+          </label>
+          <input
+            className="border w-full border-gray-300 rounded-lg p-2"
+            type="text"
+          />
+        </div>
+        <div className="mt-2">
+          <label className="font-medium mb-2 block" htmlFor="price">
+            Price
+          </label>
+          <input
+            className="border w-full border-gray-300 rounded-lg p-2"
+            type="text"
+          />
+        </div>
+        <div className="mt-2">
+          <label className="font-medium mb-2 block" htmlFor="description">
+            Description
+          </label>
+          <textarea className="border w-full border-gray-300 rounded-lg p-2" />
+        </div>
+        <div className="mt-2">
+          <label className="font-medium mb-2 block" htmlFor="category">
+            Category
+          </label>
+          <select
+            className="border w-full border-gray-300 rounded-lg p-2"
+          >
+            <option> Select category</option>
+            <option value="Laptop">Laptop</option>
+            <option value="SmartPhone">SmartPhone</option>
+          </select>
+        </div>
+        <button type="submit" className="bg-blue-500 py-2 px-4 text-white rounded-lg mt-3">Submit</button>
+      </form>
+    </div>
+  );
+};
 
-export default FormCreateProduct
+export default FormCreateProduct;
